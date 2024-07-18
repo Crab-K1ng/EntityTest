04b4c022e83727c897cf940e296f21b7837d9887
crab-k1ng <dewey2647@gamail.com>
2024-07-18 14:15:51 +0100

    Initial commit

---
+++ src/main/java/io/github/CrabK1ng/entity_test/EntityTest.java
@@ -0,0 +1,16 @@
+package io.github.CrabK1ng.entity_test;
+
+import dev.crmodders.cosmicquilt.api.entrypoint.ModInitializer;
+import org.quiltmc.loader.api.ModContainer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class EntityTest implements ModInitializer {
+	public static final Logger LOGGER = LoggerFactory.getLogger("Example Mod");
+
+	@Override
+	public void onInitialize(ModContainer mod) {
+		LOGGER.info("Example Mod Initialized!");
+	}
+}
+
