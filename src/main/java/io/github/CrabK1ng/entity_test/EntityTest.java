76faeca12d1f85f81466551449db045641cfac2d
crab-k1ng <dewey2647@gamail.com>
2024-07-22 19:24:50 +0100

    cleanup

--- src/main/java/io/github/CrabK1ng/entity_test/EntityTest.java
+++ src/main/java/io/github/CrabK1ng/entity_test/EntityTest.java
@@ -1,15 +1,12 @@
 package io.github.CrabK1ng.entity_test;
 
 import dev.crmodders.cosmicquilt.api.entrypoint.ModInitializer;
-import finalforeach.cosmicreach.GameAssetLoader;
-import io.github.CrabK1ng.entity_test.entities.TestEntity;
 import org.quiltmc.loader.api.ModContainer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import java.util.ArrayList;
 import java.util.List;
 
-import static finalforeach.cosmicreach.entities.EntityCreator.registerEntityCreator;
 
 public class EntityTest implements ModInitializer {
 	public static final String MOD_ID = "test_entity";
